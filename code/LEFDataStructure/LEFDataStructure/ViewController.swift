68e137fec0795fd7261d1c8e9d09bb09e4b79446
王素燕 <wsyxyxs@126.com>
2019-07-13 09:39:37 +0800

    no message

--- code/LEFDataStructure/LEFDataStructure/ViewController.swift
+++ code/LEFDataStructure/LEFDataStructure/ViewController.swift
@@ -12,7 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+let datas = [8, 10, 20, 7, 40 , 32 , 12, 3, 19, 26]
+let sumSeg = SegmentTree<Int>(datas: datas, defaultValue: 0, merger: { $0 + $1 })
+let maxSeg = SegmentTree<Int>(datas: datas, defaultValue: 0) { (a, b) -> Int in
+    return max(a, b)
+}
+let minSeg = SegmentTree<Int>(datas: datas, defaultValue: 0) { (a, b) -> Int in
+    return min(a, b)
+}
+
+// sum query[3-5] = 79
+if let ret = sumSeg.query(ql: 3, qr: 5) {
+    print("sum query[3-5] = \(ret)")
+}
+// max query[3-5] = 40
+if let ret = maxSeg.query(ql: 3, qr: 5) {
+    print("max query[3-5] = \(ret)")
+}
+// min query[3-5] = 7
+if let ret = minSeg.query(ql: 3, qr: 5) {
+    print("min query[3-5] = \(ret)")
+}
     }
 
 
--- code/LEFDataStructure/LEFDataStructure/segmentTree/SegmentTree.swift
+++ code/LEFDataStructure/LEFDataStructure/segmentTree/SegmentTree.swift
@@ -8,6 +8,123 @@
 
 import Foundation
 
-class SegmentTree {
+class SegmentTree<T> {
+    var datas: [T]!
+    var trees: [T]!
+    var merger: (_ a: T, _ b: T) -> T
+    init(datas: [T], defaultValue: T, merger: @escaping (_ a: T, _ b: T) -> T) {
+        self.merger = merger
+        self.datas = datas
+        // trees 初始化，大小为源数据源的 4 倍
+        trees = Array(repeating: defaultValue, count: datas.count * 4)
+        // 构建线段树
+        bulidSegment(l: 0, r: datas.count-1, curIndex: 0)
+    }
+    // 构建线段树，取值范围为 l....r，curIndex 为线段树节点的位置
+    private func bulidSegment(l: Int, r: Int, curIndex: Int) {
+        if l == r {
+            // 叶子节点
+            trees[curIndex] = datas[l]
+            return
+        }
+        // 左孩子节点
+        let lchildIndex = leftChildIndex(index: curIndex)
+        // 右孩子节点
+        let rchildIndex = rightChildIndex(index: curIndex)
+        // 中间节点
+        let mid = l + (r - l) / 2
+        // 构建左子树，范围为 [ l...mid ]
+        bulidSegment(l: l, r: mid, curIndex: lchildIndex)
+        // 构建右子树，范围为 [ mid+...r ]
+        bulidSegment(l: mid+1, r: r, curIndex: rchildIndex)
+        // 通过 merger 求得左右子树节点的数据
+        trees[curIndex] = merger(trees[lchildIndex], trees[rchildIndex])
+    }
     
+    // 查询区间范围的值
+    func query(ql: Int, qr: Int) -> T? {
+        if ql < 0 || ql > qr || ql > datas.count - 1 || qr < 0 || qr > datas.count - 1 {
+            return nil
+        }
+        return query(tIndex: 0, l: 0, r: datas.count - 1, ql: ql, qr: qr)
+    }
+    
+    // [0-5] [0-2]
+    private func query(tIndex: Int, l: Int, r: Int, ql: Int, qr: Int) -> T? {
+//        print("tIndex=\(tIndex),l=\(l),r=\(r), ql=\(ql), qr=\(qr)")
+        if l == ql && r == qr {
+            return trees[tIndex]
+        }
+        
+        let mid = l + (r - l) / 2
+        let lchildIndex = leftChildIndex(index: tIndex)
+        let rchildIndex = rightChildIndex(index: tIndex)
+        
+        if qr <= mid {
+            // 查找区间都在左子树
+            return query(tIndex: lchildIndex, l: ql, r: mid, ql: ql, qr: qr)
+        } else if ql >= mid + 1 {
+            // 查找区间都在右子树
+            return query(tIndex: rchildIndex, l: mid+1, r: r, ql: ql, qr: qr)
+        }
+        
+        // 既在 left 又在 right，分开查 left 和 right，查到结构后 merger
+        let lresult = query(tIndex: lchildIndex, l: l, r: mid, ql: ql, qr: mid)
+        let rresult = query(tIndex: rchildIndex, l: mid + 1, r: r, ql: mid + 1, qr: qr)
+        if let lresult = lresult {
+            if let rresult = rresult {
+                // 查询结果需要 merger 操作
+                return merger(lresult, rresult)
+            }
+        }
+        return nil
+    }
+    
+    func set(index: Int, value: T) {
+        if index < 0 || index > datas.count {
+            return
+        }
+        // 更新源数据中的值
+        datas[index] = value
+        // 更新线段树种的值
+        set(index: index, value: value, tIndex: 0, l: 0, r: datas.count-1)
+    }
+    
+    // 更新 index 对应的值为 value，tIndex 为当前树的根节点，l、r 表示区间
+    private func set(index: Int, value: T, tIndex: Int, l: Int, r: Int) {
+        if l == r {
+            // 叶子节点
+            trees[tIndex] = value
+            return
+        }
+        let mid = l + (r-l) / 2
+        let lchildIndex = leftChildIndex(index: tIndex)
+        let rchildIndex = rightChildIndex(index: tIndex)
+        if index <= mid {
+            // 在左子树更新
+            set(index: index, value: value, tIndex: lchildIndex, l: l, r: mid)
+        } else if index >= mid+1 {
+            // 在右子树更新
+            set(index: index, value: value, tIndex: rchildIndex, l: mid + 1, r: r)
+        }
+        // 递归回朔的时候，需要更新红色节点的值，执行 merger
+        trees[tIndex] = merger(trees[lchildIndex], trees[rchildIndex])
+    }
+    
+    
+    func leftChildIndex(index: Int) -> Int {
+        return 2 * index + 1;
+    }
+    
+    func rightChildIndex(index: Int) -> Int {
+        return 2 * index + 2;
+    }
+    
+    
+    func debug() {
+        for item in trees {
+            print(item)
+        }
+    }
+
 }
